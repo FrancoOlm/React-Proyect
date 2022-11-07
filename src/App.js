6b6827c869e35981fcb8072251b9544efbf3800d
Franco Olm <franco.olm@hotmail.com>
2022-11-07 01:52:02 -0300

    Agrego error 404

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ function App() {
             <Route path='/category/:categoryId' element={<ItemListContainer />} />
             <Route path='/detail/:productId' element={<ItemDetailContainer/>}/>
             <Route path= '/cart' element={<Cart/>}/>
-            <Route path='/checkout' element={<Checkout/>}/> 
+            <Route path='/checkout' element={<Checkout/>}/>
+            <Route path='*' element={<h1>404 NOT FOUND</h1>} /> 
           </Routes>
           </CartProvider>
       </BrowserRouter>
